637a48146ec18b1d4f819076cdcd6d313111872c
poppuko-n <souta0609@icloud.com>
2024-09-15 18:39:09 +0900

    first_commit

---
+++ password_manager.sh
@@ -0,0 +1,28 @@
+パスワードマネージャーへようこそ！
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Add Password が入力された場合
+サービス名を入力してください：
+ユーザー名を入力してください：
+パスワードを入力してください：
+
+パスワードの追加は成功しました。
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Get Password が入力された場合
+サービス名を入力してください：
+## サービス名が保存されていなかった場合
+そのサービスは登録されていません。
+## サービス名が保存されていた場合
+サービス名：hoge
+ユーザー名：fuga
+パスワード：piyo
+
+次の選択肢から入力してください(Add Password/Get Password/Exit)：
+
+# Exit が入力された場合
+Thank you!
+## プログラムが終了
+
+# Add Password/Get Password/Exit 以外が入力された場合
+入力が間違えています。Add Password/Get Password/Exit から入力してください。
